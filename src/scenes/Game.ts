3af8f36dfd9f1020c3434dfe5108b75aefeb893e
Soham Zemse <22412996+zemse@users.noreply.github.com>
2023-11-19 02:36:55 +0300

    some work

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -1,254 +1,280 @@
-import Phaser from 'phaser'
-import { createHeroAnims } from '../anims/heroAnims'
-import { sceneEvents } from '../events/EventCenter'
+import Phaser from "phaser";
+import { createHeroAnims } from "../anims/heroAnims";
+import { sceneEvents } from "../events/EventCenter";
 
 export default class Game extends Phaser.Scene {
+  private cursors!: Phaser.Types.Input.Keyboard.CursorKeys;
+  private hero!: Phaser.Physics.Matter.Sprite;
+  private mainCamera!: Phaser.Cameras.Scene2D.Camera;
 
-    private cursors!: Phaser.Types.Input.Keyboard.CursorKeys
-    private hero!: Phaser.Physics.Matter.Sprite
-    private mainCamera!: Phaser.Cameras.Scene2D.Camera
+  private gameEnded: boolean;
+  private lastLocation: { x: number; y: number };
+  private playerPath!: any[];
 
-    private gameEnded: boolean
-    private lastLocation: { x: number, y: number }
-    private playerPath!: any[]
+  private map!: Phaser.Tilemaps.Tilemap;
+  private floorLayer!: Phaser.Tilemaps.TilemapLayer | null;
 
-    private map !: Phaser.Tilemaps.Tilemap
-    private floorLayer!: Phaser.Tilemaps.TilemapLayer | null
+  constructor() {
+    super("game");
+    this.lastLocation = { x: 0, y: 0 };
+    this.playerPath = [];
+    this.gameEnded = false;
+  }
 
-    constructor() {
-        super('game')
-        this.lastLocation = { x: 0, y: 0 }
-        this.playerPath = []
-        this.gameEnded = false
+  preload() {
+    this.cursors = this.input.keyboard!.createCursorKeys();
+  }
+
+  update(): void {
+    if (!this.cursors || !this.hero || this.gameEnded) {
+      return;
     }
 
-    preload() {
-
-        this.cursors = this.input.keyboard!.createCursorKeys()
-
+    if (this.playerPath.length > 200) {
+      sceneEvents.emit("game-over", this.playerPath);
+      this.hero.setVelocity(0);
+      this.hero.stop();
+      this.gameEnded = true;
+      return;
     }
 
-    update(): void {
-        if (!this.cursors || !this.hero || this.gameEnded) {
-            return
-        }
+    const SPEED = 8;
+    let Direction;
 
-        if (this.playerPath.length > 19) {
-            sceneEvents.emit('game-over', this.playerPath)
-            this.hero.setVelocity(0)
-            this.hero.stop()
-            this.gameEnded = true
-            return
-        }
+    switch (true) {
+      case this.cursors.left?.isDown:
+        this.hero.setVelocity(-SPEED, 0);
+        this.hero.anims.play("walk-left", true);
+        Direction = 2;
+        break;
+      case this.cursors.right?.isDown:
+        this.hero.setVelocity(SPEED, 0);
+        this.hero.anims.play("walk-right", true);
+        Direction = 3;
+        break;
+      case this.cursors.up?.isDown:
+        this.hero.setVelocity(0, -SPEED);
+        this.hero.anims.play("walk-up", true);
+        Direction = 1;
+        break;
+      case this.cursors.down?.isDown:
+        this.hero.setVelocity(0, SPEED);
+        this.hero.anims.play("walk-down", true);
+        Direction = 4;
+        break;
 
-        const SPEED = 8
-        let Direction;
-
-
-        switch (true) {
-
-            case this.cursors.left?.isDown:
-                this.hero.setVelocity(-SPEED, 0)
-                this.hero.anims.play('walk-left', true);
-                Direction = 2
-                break;
-            case this.cursors.right?.isDown:
-                this.hero.setVelocity(SPEED, 0)
-                this.hero.anims.play('walk-right', true);
-                Direction = 3
-                break;
-            case this.cursors.up?.isDown:
-                this.hero.setVelocity(0, -SPEED)
-                this.hero.anims.play('walk-up', true);
-                Direction = 1
-                break;
-            case this.cursors.down?.isDown:
-                this.hero.setVelocity(0, SPEED)
-                this.hero.anims.play('walk-down', true);
-                Direction = 4
-                break;
-
-            default: {
-                //ToDo: stop the character at next tile 
-                this.hero.setVelocity(0)
-                this.hero.stop()
-
-            }
-
-        }
-
-        const tile = this.floorLayer?.getIsoTileAtWorldXY(this.hero.x, this.hero.y)
-        if (tile && (this.lastLocation.x != tile.x || this.lastLocation.y != tile.y)) {
-
-            this.lastLocation = {
-                x: tile.x,
-                y: tile.y
-            }
-
-            console.log("x : ", this.lastLocation.x, "y ", this.lastLocation.y);
-
-            this.playerPath.push(Direction)
-
-        }
-
+      default: {
+        //ToDo: stop the character at next tile
+        this.hero.setVelocity(0);
+        this.hero.stop();
+      }
     }
 
-    getDirection(oldLoc: { x: number, y: number }, newLoc: { x: number, y: number }): number {
-        /*
+    const tile = this.floorLayer?.getIsoTileAtWorldXY(this.hero.x, this.hero.y);
+    if (
+      tile &&
+      (this.lastLocation.x != tile.x || this.lastLocation.y != tile.y)
+    ) {
+      this.lastLocation = {
+        x: tile.x,
+        y: tile.y,
+      };
+
+      console.log("x : ", this.lastLocation.x, "y ", this.lastLocation.y);
+
+      //   hero-moved
+
+      this.playerPath.push(Direction);
+      sceneEvents.emit("hero-moved", this.playerPath.length);
+    }
+  }
+
+  getDirection(
+    oldLoc: { x: number; y: number },
+    newLoc: { x: number; y: number }
+  ): number {
+    /*
         1 - UP
         2 - LEFT
         3-RIGHT
         4 Down
         */
 
-        let direction = 0
+    let direction = 0;
 
-        if (oldLoc.x > newLoc.x) {
+    if (oldLoc.x > newLoc.x) {
+    } else {
+    }
 
-        } else {
+    if (oldLoc.y > newLoc.y) {
+    } else {
+    }
 
+    return direction;
+  }
+
+  getRootBody(body: any) {
+    if (body.parent === body) {
+      return body;
+    }
+    while (body.parent !== body) {
+      body = body.parent;
+    }
+    return body;
+  }
+
+  create() {
+    this.scene.run("game-ui");
+    const map = this.make.tilemap({ key: "map" });
+
+    this.map = map;
+
+    const walls = map.addTilesetImage("walls", "walls");
+    const floors = map.addTilesetImage("floors", "floors") ?? null;
+
+    this.floorLayer = map.createLayer("floors", floors || "");
+    const wallLayer = map.createLayer("walls", walls || "");
+
+    wallLayer?.setCollisionByProperty({ collide: true });
+
+    wallLayer?.setCullPadding(4, 4);
+    this.floorLayer?.setCullPadding(4, 4);
+
+    if (!wallLayer || !this.floorLayer) return;
+
+    this.matter.world.convertTilemapLayer(wallLayer, {});
+
+    createHeroAnims(this.anims);
+
+    this.hero = this.matter.add.sprite(2000, 2000, "hero");
+
+    this.hero.setBody({
+      height: this.hero.height * 0.3,
+      width: this.hero.width * 0.3,
+    });
+
+    this.hero.setFixedRotation();
+    this.hero.setDepth(4);
+
+    this.mainCamera = this.cameras.main.startFollow(this.hero, true);
+    this.cameras.main.setZoom(0.7, 0.7);
+
+    //Add treasure chests
+    /*
+    const chest = this.matter.add.image(2000, 3000, "chest", undefined, {
+      restitution: 1,
+      label: "chest",
+    });
+    chest.setFixedRotation();
+    chest.setStatic(true);
+    */
+    this.createChests();
+
+    this.matter.world.on("collisionstart", this.handleCollision, this);
+  }
+
+  createChests() {
+    const chectLocs = [
+      { x: 2000, y: 3000 },
+      { x: 2000, y: 3500 },
+      { x: 2500, y: 3500 },
+      { x: 2000, y: 4500 },
+      { x: 7000, y: 4000 },
+      { x: 10000, y: 5000 },
+      { x: 10000, y: 2000 },
+      { x: 7000, y: 10000 },
+    ];
+
+    for (const chestLoc of chectLocs) {
+      const chest = this.matter.add.image(
+        chestLoc.x,
+        chestLoc.y,
+        "chest",
+        undefined,
+        {
+          restitution: 1,
+          label: "chest",
         }
+      );
+      chest.setFixedRotation();
+      chest.setStatic(true);
+    }
+  }
 
+  handleCollision(event: any) {
+    for (let i = 0; i < event.pairs.length; i++) {
+      // The tile bodies in this example are a mixture of compound bodies and simple rectangle
+      // bodies. The "label" property was set on the parent body, so we will first make sure
+      // that we have the top level body instead of a part of a larger compound body.
+      const bodyA = this.getRootBody(event.pairs[i].bodyA);
+      const bodyB = this.getRootBody(event.pairs[i].bodyB);
 
-        if (oldLoc.y > newLoc.y) {
+      if (bodyA.label === "chest" || bodyB.label === "chest") {
+        const ballBody = bodyA.label === "chest" ? bodyA : bodyB;
+        const ball = ballBody.gameObject;
 
-        } else {
-
+        // A body may collide with multiple other bodies in a step, so we'll use a flag to
+        // only tween & destroy the ball once.
+        if (ball.isBeingDestroyed) {
+          continue;
         }
+        ball.isBeingDestroyed = true;
 
-        return direction;
+        this.matter.world.remove(ballBody);
+
+        this.tweens.add({
+          targets: ball,
+          flipY: true,
+          x: 0,
+          y: 50,
+          alpha: { value: 0, duration: 1000, ease: "Power1" },
+          onComplete: ((ball: any) => {
+            ball.destroy();
+          }).bind(this, ball),
+        });
+
+        sceneEvents.emit("coin-collected", ball.x, ball.y);
+        this.playerTileIndexing(ball.x, ball.y);
+      }
     }
+  }
 
-    getRootBody(body: any) {
-        if (body.parent === body) { return body; }
-        while (body.parent !== body) {
-            body = body.parent;
-        }
-        return body;
-    }
+  screenToTileCoordinates(x: number, y: number) {
+    // Convert screen coordinates to world coordinates
+    let worldX = x - this.scale.width / 2 + this.mainCamera.scrollX;
+    let worldY = y - this.mainCamera.scrollY;
 
-    create() {
-        this.scene.run('game-ui')
-        const map = this.make.tilemap({ key: "map" })
+    const tileHeight = 128,
+      tileWidth = 256;
 
-        this.map = map
+    // Convert world coordinates to tile coordinates
+    let tileX =
+      Math.floor(worldY / tileHeight + worldX / tileWidth) -
+      this.map.widthInPixels / (2 * tileWidth);
+    let tileY =
+      Math.floor(worldY / tileHeight - worldX / tileWidth) +
+      this.map.heightInPixels / (2 * tileHeight);
 
-        const walls = map.addTilesetImage('walls', 'walls')
-        const floors = map.addTilesetImage('floors', 'floors') ?? null
+    return { x: tileX, y: tileY };
+  }
 
-        this.floorLayer = map.createLayer("floors", floors || "")
-        const wallLayer = map.createLayer("walls", walls || "")
+  isoToTilePos(isoX: number, isoY: number) {
+    const tileWidthHalf = 256 / 2;
+    const tileHeightHalf = 128 / 2;
 
-        wallLayer?.setCollisionByProperty({ collide: true })
+    const tileX = Math.floor(isoX / tileWidthHalf + isoY / tileHeightHalf) - 1;
+    const tileY = Math.floor(-isoX / tileWidthHalf + isoY / tileHeightHalf);
 
-        wallLayer?.setCullPadding(4, 4)
-        this.floorLayer?.setCullPadding(4, 4)
+    return { x: tileX, y: tileY };
+  }
 
-        if (!wallLayer || !this.floorLayer)
-            return
+  playerTileIndexing(x: number, y: number) {
+    const playerTilePos = this.isoToTilePos(x, y);
 
-        this.matter.world.convertTilemapLayer(wallLayer, {})
+    const tile = this.floorLayer?.getIsoTileAtWorldXY(x, y);
+    // Get the tile at player's position from layer data
+    // const tile = this.floorLayer?.getTileAt(playerTilePos.x, playerTilePos.y);
 
-        createHeroAnims(this.anims)
-
-        this.hero = this.matter.add.sprite(2000, 2000, "hero")
-
-        this.hero.setBody({
-            height: this.hero.height * 0.3,
-            width: this.hero.width * 0.3,
-        })
-
-        this.hero.setFixedRotation()
-        this.hero.setDepth(4)
-
-        this.mainCamera = this.cameras.main.startFollow(this.hero, true)
-        this.cameras.main.setZoom(0.7, 0.7)
-
-
-        //Add treasure chests
-        const chest = this.matter.add.image(2000, 3000, 'chest', undefined, { restitution: 1, label: 'chest' });
-        // chest.setBody({
-        //     height: chest.height * 0.8,
-        //     width: chest.width * 0.8,
-        // })
-        chest.setFixedRotation()
-        chest.setStatic(true)
-
-        this.matter.world.on('collisionstart', this.handleCollision, this)
-
-    }
-
-    handleCollision(event: any) {
-        for (let i = 0; i < event.pairs.length; i++) {
-            // The tile bodies in this example are a mixture of compound bodies and simple rectangle
-            // bodies. The "label" property was set on the parent body, so we will first make sure
-            // that we have the top level body instead of a part of a larger compound body.
-            const bodyA = this.getRootBody(event.pairs[i].bodyA);
-            const bodyB = this.getRootBody(event.pairs[i].bodyB);
-
-            if (bodyA.label === 'chest' || bodyB.label === 'chest') {
-                const ballBody = bodyA.label === 'chest' ? bodyA : bodyB;
-                const ball = ballBody.gameObject;
-
-                // A body may collide with multiple other bodies in a step, so we'll use a flag to
-                // only tween & destroy the ball once.
-                if (ball.isBeingDestroyed) {
-                    continue;
-                }
-                ball.isBeingDestroyed = true;
-
-                this.matter.world.remove(ballBody);
-
-                this.tweens.add({
-                    targets: ball,
-                    flipY: true,
-                    x: 0,
-                    y: 50,
-                    alpha: { value: 0, duration: 1000, ease: 'Power1' },
-                    onComplete: ((ball: any) => { ball.destroy(); }).bind(this, ball)
-                });
-
-
-                sceneEvents.emit('coin-collected', ball.x, ball.y)
-                this.playerTileIndexing(ball.x, ball.y)
-            }
-        }
-
-    }
-
-
-    screenToTileCoordinates(x: number, y: number) {
-        // Convert screen coordinates to world coordinates
-        let worldX = x - this.scale.width / 2 + this.mainCamera.scrollX;
-        let worldY = y - this.mainCamera.scrollY;
-
-        const tileHeight = 128, tileWidth = 256;
-
-        // Convert world coordinates to tile coordinates
-        let tileX = Math.floor((worldY / tileHeight) + (worldX / tileWidth)) - this.map.widthInPixels / (2 * tileWidth);
-        let tileY = Math.floor((worldY / tileHeight) - (worldX / tileWidth)) + this.map.heightInPixels / (2 * tileHeight);
-
-        return { x: tileX, y: tileY };
-    }
-
-    isoToTilePos(isoX: number, isoY: number) {
-        const tileWidthHalf = 256 / 2;
-        const tileHeightHalf = 128 / 2;
-
-        const tileX = Math.floor(isoX / tileWidthHalf + isoY / tileHeightHalf) - 1;
-        const tileY = Math.floor(-isoX / tileWidthHalf + isoY / tileHeightHalf);
-
-        return { x: tileX, y: tileY };
-    }
-
-    playerTileIndexing(x: number, y: number) {
-        const playerTilePos = this.isoToTilePos(x, y);
-
-        const tile = this.floorLayer?.getIsoTileAtWorldXY(x, y)
-        // Get the tile at player's position from layer data
-        // const tile = this.floorLayer?.getTileAt(playerTilePos.x, playerTilePos.y);
-
-        console.debug({ tile, playerTilePos })
-
-    }
+    console.debug({ tile, playerTilePos });
+  }
 }
--- src/scenes/GameUI.ts
+++ src/scenes/GameUI.ts
@@ -1,90 +1,100 @@
-import Phaser from 'phaser'
-import { sceneEvents } from '../events/EventCenter'
+import Phaser from "phaser";
+import { sceneEvents } from "../events/EventCenter";
 
 export default class GameUI extends Phaser.Scene {
-    score: number
-    scoreText!: Phaser.GameObjects.Text
+  score: number;
+  scoreText!: Phaser.GameObjects.Text;
+  remainingSteps!: number;
+  remainingStepsText!: Phaser.GameObjects.Text;
 
-    private music: boolean
+  private music: boolean;
 
-    constructor() {
-        super({ key: 'game-ui' })
-        this.score = 0
-        this.music = false
+  constructor() {
+    super({ key: "game-ui" });
+    this.score = 0;
+    this.remainingSteps = 200;
+    this.music = false;
+  }
 
-    }
+  create() {
+    sceneEvents.on("hero-moved", this.handleHeroMove, this);
+    sceneEvents.on("coin-collected", this.handleCoinCollection, this);
+    sceneEvents.once("game-over", this.handleEndGame, this);
 
-    create() {
+    // this.scoreText = "Coins"
+    this.scoreText = this.add.text(20, 50, `Score: ${this.score}`, {
+      fontSize: 40,
+      color: "black",
+    });
 
-        sceneEvents.on('coin-collected', this.handleCoinCollection, this)
-        sceneEvents.once('game-over', this.handleEndGame, this)
+    this.remainingStepsText = this.add.text(
+      20,
+      80,
+      `Steps Remaining: ${this.remainingSteps}`,
+      {
+        fontSize: 40,
+        color: "black",
+      }
+    );
 
-        // this.scoreText = "Coins"
-        this.scoreText = this.add.text(20, 50, `Score: ${this.score}`, {
-            fontSize: 40,
-            color: "black",
-        })
+    const paddings = 5;
+    const button = this.add
+      .text(70, 150, "Music: ON", {
+        // fontFamily: 'Roboto',
+        backgroundColor: "grey",
+        fontSize: "16px",
+        padding: {
+          left: paddings,
+          right: paddings,
+          top: paddings,
+          bottom: paddings,
+        },
+        color: "white",
+        align: "center",
+      })
+      .setOrigin(0.5);
+    button.setInteractive({ useHandCursor: true });
 
-        const paddings = 5
-        const button = this.add.text(70, 100, 'Music: ON', {
-            // fontFamily: 'Roboto',
-            backgroundColor:"grey",
-            fontSize: '16px',
-            padding: 
-            {
-                left: paddings,
-                right: paddings,
-                top: paddings,
-                bottom: paddings
-            },
-            color: "white",
-            align: 'center',
+    const music = this.sound.add("theme", {
+      loop: true,
+    });
 
-        }).setOrigin(0.5);
-        button.setInteractive({ useHandCursor: true });
+    button.on("pointerdown", () => {
+      this.music = !this.music;
 
-        const music = this.sound.add('theme', {
-            loop: true
-        });
+      button.setText(`Music: ${this.music ? "OFF" : "ON"}`);
+      if (this.music) {
+        if (music.isPaused) music.resume();
+        else music.play();
+      } else {
+        music.pause();
+      }
+    });
+  }
 
-        button.on('pointerdown', () => {
-            this.music = !this.music;
+  handleHeroMove(movedSteps: number) {
+    this.remainingSteps = 50 - movedSteps < 0 ? 0 : 200 - movedSteps;
+    this.remainingStepsText.setText(`Steps Remaining: ${this.remainingSteps}`);
+  }
 
-            button.setText(`Music: ${this.music ? 'OFF' : 'ON'}`)
-            if (this.music) {
-                if (music.isPaused)
-                    music.resume();
-                else
-                    music.play();
-            } else {
-                music.pause()
-            }
+  handleCoinCollection(x: number, y: number) {
+    console.debug(`COIN x: ${x} , y : ${y}`);
+    this.score++;
+    this.sound.playAudioSprite("sfx", "ping");
+    this.scoreText.setText(`Score: ${this.score}`);
+  }
 
-        });
-
-
-
-
-
-    }
-
-    handleCoinCollection(x: number, y: number) {
-        console.debug(`COIN x: ${x} , y : ${y}`)
-        this.score++;
-        this.sound.playAudioSprite('sfx', "ping");
-        this.scoreText.setText(`Score: ${this.score}`)
-    }
-
-    handleEndGame(playerPath:number[]) {
-        this.sound.playAudioSprite('sfx', "death");
-        this.add.text(200, 200, `Game Over`, {
-            fontSize: 100,
-            color: "black",
-        })
-        //ToDo: @soham call service here, 
-        //player path is your variable
-        console.log({playerPath});
-        
-    }
-
+  async handleEndGame(playerPath: number[]) {
+    this.sound.playAudioSprite("sfx", "death");
+    this.add.text(200, 200, `Game Over`, {
+      fontSize: 100,
+      color: "black",
+    });
+    //ToDo: @soham call service here,
+    let result = await fetch(
+      `http://localhost:1234/prove?solution=${playerPath.join("")}`
+    );
+    //player path is your variable
+    console.log({ playerPath, result });
+  }
 }
