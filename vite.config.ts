f0d24c5e30a71e0dc8586fb41a51d123a55c8073
himank jain <himankjain@yahoo.in>
2024-01-25 14:11:47 +0530

    vite build fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+	build: {
+		target: 'esnext' //browsers can handle the latest ES features
+	  },
 	optimizeDeps: {
 		esbuildOptions: {
 			target: 'esnext'
